7b47fbacb4b7d849d5a7602e9113915c734bd428
Violetta Lamb <violettalamb05@gmail.com>
2025-01-30 11:56:16 -0600

    Created our First C++ file for Coding 1
    We are learning to use github, good commit messages and descriptions matter for finding bugs.

---
+++ Coding1.2.cpp
@@ -0,0 +1,33 @@
+// Coding1.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "What is your name?\n";
+    cin >> name;
+
+    if (name == "Blake" or name == "Violetta")
+    {
+        cout << "Hello Creator!" << endl;
+    }
+    else
+    {
+        cout << "Hello " << name << "!" << endl;
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
